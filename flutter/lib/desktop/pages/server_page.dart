850058b79a6d9ac68efefe7c9d51b3ad5dcb58ac
gon4u <75760993+gon4u@users.noreply.github.com>
2023-06-15 14:14:13 +0100

    Update server_page.dart gradient

--- flutter/lib/desktop/pages/server_page.dart
+++ flutter/lib/desktop/pages/server_page.dart
@@ -353,8 +353,8 @@ class _CmHeaderState extends State<_CmHeader>
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
           colors: [
-            Color(0xff00bfe1),
-            Color(0xff0071ff),
+            Color(0xff05e8c5),
+            Color(0xff038773),
           ],
         ),
       ),
